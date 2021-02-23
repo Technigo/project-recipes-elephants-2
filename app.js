a0260ee39a18b80280dcb5bce5eef7a570604320
petercreations <hello@petercreations.com>
2021-02-23 10:42:25 +0100

    added event listeners

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
 const recipeWrapper = document.getElementById('recipe-wrapper')
 
 
-  //APIs for each food category
-  const chickenRecipesApi = ('https://api.edamam.com/search?q=chicken&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
-  const beefRecipesApi = ('https://api.edamam.com/search?q=beef&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
-  const fishRecipesApi = ('https://api.edamam.com/search?q=fish&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
-  const vegRecipesApi = ('https://api.edamam.com/search?q=vegetarian&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
+//APIs for each food category
+const chickenRecipesApi = ('https://api.edamam.com/search?q=chicken&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
+const beefRecipesApi = ('https://api.edamam.com/search?q=beef&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
+const fishRecipesApi = ('https://api.edamam.com/search?q=fish&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
+const vegRecipesApi = ('https://api.edamam.com/search?q=vegetarian&app_id=03cb902c&app_key=2f60fc9619cfcd069d03e29e2509365f');
 
 // Buttons 
 const chickenBtn = document.querySelector("#chicken-button")
-const beefBtn = document.querySelector("#beef-btn")
-const fishBtn = document.querySelector("#fish-btn")
-const vegBtn = document.querySelector("#veg-btn")
+const beefBtn = document.querySelector("#beef-button")
+const fishBtn = document.querySelector("#fish-button")
+const vegBtn = document.querySelector("#veg-button")
 
-// Store Json Data in variable
-// let jsondata;   
-// fetch(chickenRecipesApi)
-//         .then(
-//         function(u){ return u.json();}
-//       )
-//       .then(
-//         function(json){
-//           jsondata = json;
-//           console.log(jsondata);
-//           jsondata.hits.forEach((item) => {
-//                     recipeWrapper.innerHTML += `
-//                   <h1>${item.recipe.label}</h1>
-//                   <img src="${item.recipe.image}"/>
-//                   <p>${item.recipe.ingredientLines}</p>
-//                   <p>${item.recipe.totalTime}min</p>
-//                   `
-//         }
-//       )
-//     })
+
 
 let category = null;
 //Fetch Chicken Recipes from real API
-  const fetchChickenRecipes = () => {
+  const fetchRecipes = (category) => {
     fetch(category)
       .then((response) => {
         return response.json();
@@ -57,15 +38,21 @@ let category = null;
      
      });
   };
-  fetchChickenRecipes();
+  fetchRecipes();
 
 // Switch between onClick category choices which recipes to display 
 
 chickenBtn.addEventListener("click", () => {
-  category = chickenRecipesApi;
+  fetchRecipes(chickenRecipesApi);
 })
 beefBtn.addEventListener("click", () => {
-  category = beefRecipesApi;
+  fetchRecipes(beefRecipesApi);
+})
+fishBtn.addEventListener("click", () => {
+  fetchRecipes(fishRecipesApi);
+})
+vegBtn.addEventListener("click", () => {
+  fetchRecipes(vegRecipesApi);
 })
 
 
